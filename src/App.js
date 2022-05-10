ccbb37dc7d77501301cb64aada550607ff0ca7da
adrianofranca111 <adriano.franca@discente.ufma.br>
2022-05-10 10:50:49 -0300

    modulo 3 aula 24

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
-import { Text } from 'react-native'
- 
-export default() => <Text>Primeiro Componente</Text>
+import { SafeAreaView, StyleSheet } from 'react-native'
 
+import MinMax from './components/MinMax'
+//import CompPadrao, { Comp1, Comp2 } from './components/Mult'
+//import Primeiro from './components/Primeiro'
+
+export default() => (
+    <SafeAreaView style={style.App}>
+        <MinMax min={3} max={20}/>
+        {/*<CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />*/}
+    </SafeAreaView>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: "center", 
+        alignItems: "center",
+        padding: 20
+    }
+})
---
+++ src/components/estilo.js
@@ -0,0 +1,8 @@
+import { } from 'react-native'
+
+export default StyleSheet.create({
+    Titulo: {
+        fontSize: 24,
+        textAlign: "center",
+    }
+})
