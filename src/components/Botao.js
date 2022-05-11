bfbfd9250919503836c83fe547d7e2c3fb962fd4
adrianofranca111 <adriano.franca@discente.ufma.br>
2022-05-11 12:04:26 -0300

    componente com estado aula 29

---
+++ src/components/Botao.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+export default props => {
+
+    function executar(){
+        console.warn('Executado #01')
+    }
+
+    return(
+        <>
+            <Button 
+            title='Executar #01'
+            onPress={executar}
+            />
+            <Button 
+            title='Executar #02'
+            onPress={function(){
+                console.warn('Executado #02')
+            }}
+            />
+        </>
+    )
+}
